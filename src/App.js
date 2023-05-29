0f4da548a47813764ed412f336dc7749677dac21
Mohammad Zaki <mz.26925787@gmail.com>
2023-05-29 05:41:26 -0700

    Add Home component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/Home';
 
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
 <Header/>
+<Home/>
 <Footer/>
     </>
   )
