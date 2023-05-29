83ee7e7e50b05cb0ac5c259e46c0197724efad6a
Mohammad Zaki <mz.26925787@gmail.com>
2023-05-29 05:35:01 -0700

    Add header and footer components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import './App.css';
-import Card from './components/Card/Card';
-import Footer from './components/Footer/Footer';
-import Navbar from './components/Navbar/navbar';
-import Skills from './components/Skills/Skills';
-import Slider from './components/Slider/Slider';
-import About from './components/about/About';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Slider/>
-    <About/>
-    <Skills width="50%" name="Html"/>
-    <Skills width="70%" name="Css"/>
-    <Skills width="80%" name="JavaScript"/>
-    .<div class="container1">
-    <Card color="blue"/>
-    <Card color="yellow"/>
-    <Card color="green"/>
-    <Card color="black"/>
-    </div>
-    <Footer/>
+<Header/>
+<Footer/>
     </>
   )
 }
