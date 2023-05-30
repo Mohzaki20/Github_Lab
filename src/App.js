af68d3add847d8fdd66117c125201720c6e42914
Mohammad Zaki <mz.26925787@gmail.com>
2023-05-30 12:57:28 -0700

    add moataz branch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
+import Login from './components/Login';
+import { Contact } from './contact/contact';
 
 
 
